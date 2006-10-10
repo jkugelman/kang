9f2af852b3d8402e2382a366c09f80eb80909674
john.kugelman <john.kugelman@eedd176b-c91e-0410-9df4-1f401b7bbd07>
2006-10-10 05:02:09 +0000

    Ported Log classes from C#. Grabbed grammar.xml.

---
+++ trunk/Java/src/name/kugelman/john/kang/compiler/Program.java
@@ -0,0 +1,47 @@
+package name.kugelman.john.kang.compiler;
+
+import java.io.*;
+
+import name.kugelman.john.compiler.*;
+import name.kugelman.john.util.*;
+
+/**
+ * A Kang program. This class parses and creates executable programs from Kang
+ * source code files.
+ */
+public class Program {
+    /**
+     * Creates a new Kang program.
+     * 
+     * @param sourceFile  the name of the source file
+     * 
+     * @throws FileNotFoundException
+     *     if the input source file is not found. 
+     * @throws IOException 
+     *     if there's an error reading from the source file.
+     * @throws Parser.Exception
+     *     
+     */
+    public Program(File sourceFile)
+        throws FileNotFoundException, Parser.Exception, IOException
+    {
+        try {
+            InputStream stream    = Program.class.getResourceAsStream("grammar.xml");
+            Grammar     grammar   = Grammar.fromXML(stream);
+            Parser      parser    = new Parser(grammar);
+            ParseTree   parseTree = parser.parse(new Tokenizer(new FileReader(sourceFile)));
+    
+            parseTree.print(System.out);
+        }
+        catch (InvalidGrammarException exception) {
+            Debug.logError(exception);
+            throw new RuntimeException(exception);
+        }
+    }
+
+    /**
+     * Executes the program.
+     */
+    public void execute() {
+    }
+}
